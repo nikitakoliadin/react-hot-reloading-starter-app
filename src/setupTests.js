d57b5f9101fd2bb511aec950df41e85a2f3b70d9
Nikita Koliadin <qThegamEp@gmail.com>
2018-12-15 14:30:13 +0200

    Created setupTests.js

---
+++ src/setupTests.js
@@ -0,0 +1,5 @@
+// react-testing-library renders your components to document.body,
+// this will ensure they're removed after each test.
+import 'react-testing-library';
+// this adds jest-dom's custom assertions.
+import 'jest-dom/extend-expect';
